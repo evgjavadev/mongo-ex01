dd4e97d95bfc866bde372c9e35ae461865fcfb72
Evgeny Chebotarev <evgeny_chebotarev@epam.com>
2021-11-28 15:31:00 +0300

    Clean up. Move db host to compose file

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ COPY target/mongo-todo-0.0.1-SNAPSHOT.jar /app/application.jar
 WORKDIR /app
 
 # ENV MONGO_DB_HOST=host.docker.internal
-ENV MONGO_DB_HOST=mongodb_5
+# ENV MONGO_DB_HOST=mongodb_5
 
 # EXPOSE 8080
 
--- src/main/java/com/mongo/example01/todo/entity/ToDoModel.java
+++ src/main/java/com/mongo/example01/todo/entity/ToDoModel.java
@@ -6,12 +6,6 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-
-//@Entity
 @Document
 @NoArgsConstructor
 @Getter
@@ -19,7 +13,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ToDoModel {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
